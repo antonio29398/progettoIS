ef56b71db3b3eb2b9ad6bb3177337911b29d7347
antonio <antonionapolitano58@yahoo.it>
2021-12-05 02:27:05 +0100

    Progetto v2 e modifiche Vpp

--- Implementazione/src/GestionaleInizializza/Main.java
+++ Implementazione/src/GestionaleInizializza/Main.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 
 import Gestionale.entity.*;
 import Gestionale.Controller.*;
-import Gestionale.Database.*;
+
 
 
 public class Main {
@@ -16,9 +16,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		
+		//istanze di classe control per usare i suoi metodi
 		ControlUtente controlUtente = null;
 		ControlGestore controlgestore = null;
 		
+		
+		//popolamento del database
 		Volo volo = null;
 		Volo volo1 = null;
 		Volo volo2 = null;
@@ -34,29 +38,28 @@ public class Main {
 			controlUtente =new ControlUtente();
 			controlgestore = new ControlGestore();
 			
-			//compagnia 
+			//oggetti classe compagnia
 			int id = 1; 
 			String nome = "alitalia";
-			
 			c = new Compagnia(nome,id);
 			
-			 id = 2; 
-			 nome = "volissimo";
 			
+			id = 2; 
+			nome = "volissimo";
 			c1 = new Compagnia(nome,id);
 			
 			
-			 id = 3; 
-			 nome = "volissimissimo";
-			
+			id = 3; 
+			nome = "volissimissimo";
 			c2 = new Compagnia(nome,id);
+			
 			
 			controlgestore.aggiungiCompagnia(c);
 			controlgestore.aggiungiCompagnia(c1);
 			controlgestore.aggiungiCompagnia(c2);
 			controlgestore.visualizzaCompagnia();
 			
-			//insert voli
+			//oggetti classe volo
 			int Idvolo = 1;
 			int Idaereo = 1;
 			int Idtratta = 1;
@@ -67,40 +70,37 @@ public class Main {
 			int Posti = 100;
 			int BagagliBordoDispo = 200;
 			int BagagliStivaDispo = 300;
-			volo = new Volo( Idvolo,Idaereo,Idtratta,Giorno,OraArrivo,OraPartenza,Prezzo,BagagliBordoDispo,BagagliStivaDispo,Posti);
+	
+			volo = new Volo( Idvolo,Idtratta,Idaereo,Giorno,OraArrivo,OraPartenza,Prezzo,BagagliBordoDispo,BagagliStivaDispo,Posti);
 
-			 Idvolo = 2;
-			 Idaereo = 2;
-			 Idtratta=2;
-			 Giorno = 12122012 ;
-			 OraArrivo = 1930;
-			 OraPartenza = 1900;
-			 Prezzo = 600;
-			 Posti = 1300;
-			 BagagliBordoDispo = 200;
-			 BagagliStivaDispo = 300;
 			
-			volo1 = new Volo( Idvolo,Idaereo,Idtratta,Giorno,OraArrivo,OraPartenza,Prezzo,BagagliBordoDispo,BagagliStivaDispo,Posti);
-		
-		
-			
-			 Idvolo = 3;
-			 Idaereo = 3;
-			 Idtratta =3;
-			 Giorno = 21022021;
-			 OraArrivo = 2100;
-			 OraPartenza = 1500;
-			 Prezzo = 1600;
-			 Posti = 1300;
-			 BagagliBordoDispo = 200;
-			 BagagliStivaDispo = 300;
-			
-			volo2 = new Volo( Idvolo,Idaereo,Idtratta,Giorno,OraArrivo,OraPartenza,Prezzo,BagagliBordoDispo,BagagliStivaDispo,Posti);
-		
+			Idvolo = 2;
+			Idaereo = 2;
+			Idtratta=2;
+			Giorno = 12122012 ;
+			OraArrivo = 1930;
+			OraPartenza = 1900;
+			Prezzo = 600;
+			Posti = 1300;
+			BagagliBordoDispo = 200;
+			BagagliStivaDispo = 300;
+			 
+			volo1 = new Volo( Idvolo,Idtratta,Idaereo,Giorno,OraArrivo,OraPartenza,Prezzo,BagagliBordoDispo,BagagliStivaDispo,Posti);
 		
 			
+			Idvolo = 3;
+			Idaereo = 3;
+			Idtratta =3;
+			Giorno = 21022021;
+			OraArrivo = 2100;
+			OraPartenza = 1500;
+			Prezzo = 1600;
+			Posti = 1300;
+			BagagliBordoDispo = 200;
+			BagagliStivaDispo = 300;
 			
-		
+			volo2 = new Volo( Idvolo,Idtratta,Idaereo,Giorno,OraArrivo,OraPartenza,Prezzo,BagagliBordoDispo,BagagliStivaDispo,Posti);
+			
 		
 			controlUtente.AggiungiVolo(volo);
 			controlUtente.AggiungiVolo(volo1);
